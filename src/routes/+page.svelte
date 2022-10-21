a667f1d1637ed3fb83c55aa9c44dbe9145cb408d
yghong <ghddbsrmsz@naver.com>
2022-10-21 18:07:02 +0900

    svelte-todo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+  import './style.css'
+  import List from './List.svelte'
+  let value;
+</script>
+
+<div class="wrap">
+  <div class="input_wrap">
+    <input bind:value={value} type="text">
+    <button>확인</button>
+  </div>
+  <div class="box">
+    <List></List>
+  </div>
+</div>
+
+<style>
+  input {
+    font-size: 16px;
+    border: none;
+    border-bottom: 1px solid #000;
+    background: none;
+  }
+  input:focus{
+    outline: none;
+  }
+  .wrap {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    width: 500px;
+    height: 500px;
+  }
+  .input_wrap {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 10%;
+    gap: 30px;
+  }
+  button {
+    background: dodgerblue;
+    padding: 10px;
+    border-radius: 10px;
+    border: none;
+    width: 100px;
+  }
+  .input_wrap input{
+    width: 70%;
+    padding: 5px 10px;
+  }
+</style>
---
+++ src/routes/List.svelte
@@ -0,0 +1,25 @@
+<script>
+  import {onMount} from 'svelte'
+
+  let isOk = false
+
+  
+</script>
+
+<div class="list_wrap">
+<p class="list_item" class:on={isOk}>
+  <slot>asdasd</slot>
+</p>
+<input type="button" on:click={() => {isOk = !isOk}} value="확인">
+<div>삭제</div>
+
+</div>
+
+<style>
+.list_item {
+  min-width: 100px;
+}
+.on {
+  text-decoration: line-through;
+}
+</style>
