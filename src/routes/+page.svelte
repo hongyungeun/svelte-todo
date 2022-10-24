adc53d7297a9194c207c2a83bafc36331cd6660a
yghong <ghddbsrmsz@naver.com>
2022-10-24 15:37:56 +0900

    todo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,62 @@
 <script>
+  import {onMount} from 'svelte'
   import './style.css'
   import List from './List.svelte'
+  
+  
   let value;
+  let id=3
+  
+  let text =''
+  let propVal = [{
+    id:1,
+    value:'1번할일',
+    
+  },{
+    id:2,
+    value:'2번할일',
+    
+  }
+]
+
+  $: listLength= propVal.length;
+  
+  function todoPlus (){
+    if(value == ''){
+      alert('할일 입력하고 확인누르쇼')
+    }else{
+      propVal.push({
+        id,
+        value
+      })
+      propVal = propVal
+      value=''
+      id +=1
+      
+    }
+  }
+
+  console.log(listLength)
 </script>
 
 <div class="wrap">
   <div class="input_wrap">
-    <input bind:value={value} type="text">
-    <button>확인</button>
+    <p class="title">To Do List</p>
+    <div class="write_input">
+      <input bind:value={value} type="text" placeholder="할일">
+      <button on:click={todoPlus}>확인</button>
+    </div>
+  </div>
+  <div class="length_wrap">
+    현재 남은 할일 갯수 : 
+    {#if listLength}
+      {listLength}
+    {/if}
   </div>
   <div class="box">
-    <List></List>
+    {#each propVal as item }
+      <List propVal = {propVal} item={item}></List>
+    {/each}
   </div>
 </div>
 
@@ -18,8 +64,9 @@
   input {
     font-size: 16px;
     border: none;
-    border-bottom: 1px solid #000;
-    background: none;
+    background: white;
+    border-radius: 20px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.299);
   }
   input:focus{
     outline: none;
@@ -33,9 +80,16 @@
     height: 500px;
   }
   .input_wrap {
+    margin-bottom: 30px;
+  }
+  .title {
+    font-size: 30px;
+    font-weight: bold;
+  }
+  .write_input{
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
     margin-top: 10%;
     gap: 30px;
   }
@@ -45,9 +99,17 @@
     border-radius: 10px;
     border: none;
     width: 100px;
+    font-weight: bold;
+    font-size: 16px;
+    color: white;
+    cursor: pointer;
+  }
+  button:active {
+    opacity: 0.7;
   }
   .input_wrap input{
-    width: 70%;
-    padding: 5px 10px;
+    width: 80%;
+    padding:  15px;
+
   }
 </style>
--- src/routes/List.svelte
+++ src/routes/List.svelte
@@ -1,23 +1,41 @@
 <script>
   import {onMount} from 'svelte'
-
+  import Page from './+page.svelte'
+  export let propVal
+  export let item;
   let isOk = false
-
-  
+  console.log(item)
+  console.log(propVal)
+  function deleteTodo(){
+    propVal = propVal.filter(t =>{console.log(t.id !== item.id)
+       t.id !== item.id})
+    this.parentNode.remove()
+    console.log(this.id)
+  }
 </script>
 
 <div class="list_wrap">
-<p class="list_item" class:on={isOk}>
-  <slot>asdasd</slot>
-</p>
-<input type="button" on:click={() => {isOk = !isOk}} value="확인">
-<div>삭제</div>
+  <input type="checkbox" on:click={()=>isOk = !isOk}>
+  <p class="list_item" class:on={isOk}>
+    {item.value}
+  </p>
+  
+  
+  <!-- <input type="button" on:click={() => {isOk = !isOk}} value="확인"> -->
+<div on:click={deleteTodo}>삭제</div>
 
 </div>
 
 <style>
 .list_item {
   min-width: 100px;
+  width: 80%;
+}
+.list_wrap {
+  display: flex;
+  align-self: center;
+  justify-content: space-between;
+  width: 100%;
 }
 .on {
   text-decoration: line-through;
